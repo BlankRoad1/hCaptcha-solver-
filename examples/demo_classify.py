0d393c391113e179d82a0dcbe690d18bc1afd0e3
Blank Road <123190492+BlankRoad1@users.noreply.github.com>
2023-02-05 21:09:42 +0600

    Update demo_classify.py

--- examples/demo_classify.py
+++ examples/demo_classify.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Time       : 2022/9/23 17:28
-# Author     : QIN2DIM
-# Github     : https://github.com/QIN2DIM
+# Author     : BlankRoad
+# Github     : https://github.com/BlankRoad1
 # Description:
 import os
 from pathlib import Path

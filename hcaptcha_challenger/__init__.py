adcaae0a8bd5dbf38e3ba8107083622be53cd87a
Blank Road <123190492+BlankRoad1@users.noreply.github.com>
2023-02-05 21:13:09 +0600

    Update __init__.py

--- hcaptcha_challenger/__init__.py
+++ hcaptcha_challenger/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Time       : 2022/2/15 17:43
 # Author     : QIN2DIM
-# Github     : https://github.com/QIN2DIM
+# Github     : https://github.com/BlankRoad1
 # Description:
 import os
 import shutil

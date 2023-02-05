81dec310fafab367e5e473249d031331446c1726
Blank Road <123190492+BlankRoad1@users.noreply.github.com>
2023-02-05 21:14:48 +0600

    Update kernel.py

--- hcaptcha_challenger/_solutions/kernel.py
+++ hcaptcha_challenger/_solutions/kernel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Time       : 2022/4/30 22:34
-# Author     : QIN2DIM
-# Github     : https://github.com/QIN2DIM
+# Author     : BlankRoad
+# Github     : https://github.com/BlankRoad1
 # Description:
 import json
 import os
